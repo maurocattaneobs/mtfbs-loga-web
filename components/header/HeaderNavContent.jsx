1e012d5a97a065ebf2d1f22b5c26d752e8010ede
Mauro Cattaneo 🍺 <mauro.cattaneo@mtfbs.com>
2024-05-07 16:16:09 +0200

    Test vercel

--- components/header/HeaderNavContent.jsx
+++ components/header/HeaderNavContent.jsx
@@ -63,7 +63,7 @@ const HeaderNavContent = () => {
             } dropdown has-mega-menu`}
             id="has-mega-menu"
           >
-            <span>Find Jobs</span>
+            <span>Find Jobs 2</span>
             <div className="mega-menu">
               <div className="mega-menu-bar row">
                 {findJobItems.map((item) => (
