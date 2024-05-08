b300adaf2b8ad56a2e3e0753348c66584004a107
Mauro Cattaneo 🍺 <mauro.cattaneo@mtfbs.com>
2024-05-08 17:09:27 +0200

    Update config with prod url

--- services/config.js
+++ services/config.js
@@ -1,7 +1,6 @@
 const appConfig = {
-  logaUrlProd:
+  logaUrl:
     "https://mtfbusi.pi-asp.de/bewerber-web/public/JobPortalData?companyEid=AAA72&tenant=&lang=IS&jobPortal=HOMEPAGE",
-  logaUrl: "http://www.bnimpianti.ch/assets/mock.xml",
 };
 
 export default appConfig;
