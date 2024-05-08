131f61464a9d7ed952994147368be7dff830458e
Mauro Cattaneo 🍺 <mauro.cattaneo@mtfbs.com>
2024-05-08 15:41:09 +0200

    Update xml url for test

--- services/config.js
+++ services/config.js
@@ -1,6 +1,8 @@
 const appConfig = {
-  logaUrl:
+  logaUrlProd:
     "https://mtfbusi.pi-asp.de/bewerber-web/public/JobPortalData?companyEid=AAA72&tenant=&lang=IS&jobPortal=HOMEPAGE",
+  logaUrl:
+    "https://raw.githubusercontent.com/mauro-cattaneo/mock/b3cc9ab4318faea4e29b880fa9fcb473a15c33c4/mock.xml",
 };
 
 export default appConfig;
