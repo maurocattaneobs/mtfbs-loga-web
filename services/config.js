2f52d6d3131e984d2eeae764f7ca062def7c26c3
Mauro Cattaneo 🍺 <mauro.cattaneo@mtfbs.com>
2024-06-24 17:36:49 +0200

    Update url to test env

--- services/config.js
+++ services/config.js
@@ -1,6 +1,7 @@
 const appConfig = {
   logaUrl:
-    "https://mtfbusi.pi-asp.de/bewerber-web/public/JobPortalData?companyEid=AAA72&tenant=&lang=IS&jobPortal=HOMEPAGE",
+    //"https://mtfbusi.pi-asp.de/bewerber-web/public/JobPortalData?companyEid=AAA72&tenant=&lang=IS&jobPortal=HOMEPAGE",
+    "https://mtfbusi-test.pi-asp.de/bewerber-web/public/JobPortalData?companyEid=AAA72&tenant=&lang=IS&jobPortal=HOMEPAGE",
 };
 
 export default appConfig;
