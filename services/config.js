01bb6c32010b985df63a607d161bd6be0d74b5a7
Mauro Cattaneo 🍺 <mauro.cattaneo@mtfbs.com>
2024-06-25 10:36:36 +0200

    Restore production url

--- services/config.js
+++ services/config.js
@@ -1,7 +1,6 @@
 const appConfig = {
   logaUrl:
-    //"https://mtfbusi.pi-asp.de/bewerber-web/public/JobPortalData?companyEid=AAA72&tenant=&lang=IS&jobPortal=HOMEPAGE",
-    "https://mtfbusi-test.pi-asp.de/bewerber-web/public/JobPortalData?companyEid=AAA72&tenant=&lang=IS&jobPortal=HOMEPAGE",
+    "https://mtfbusi.pi-asp.de/bewerber-web/public/JobPortalData?companyEid=AAA72&tenant=&lang=IS&jobPortal=HOMEPAGE",
 };
 
 export default appConfig;
