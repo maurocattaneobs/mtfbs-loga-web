4b6ca74cd449496b5c004a2e544dfb79b92fb30a
Mauro Cattaneo 🍺 <mauro.cattaneo@mtfbs.com>
2024-10-28 13:42:22 +0100

    update test

--- app/api/jobs/[id]/route.ts
+++ app/api/jobs/[id]/route.ts
@@ -1,12 +1,20 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 import { fetchJobs } from "../../../../services/api";
+import appConfig from "../../../config";
 export const dynamic = "force-dynamic";
 
 export async function GET(
   req: NextRequest,
   { params }: { params: { id: string } }
 ) {
-  const jobs = await fetchJobs();
+  const remoteUrl_IS = `${appConfig.logaUrl_IS}`;
+  const jobs_IS = await fetchJobs(remoteUrl_IS);
+
+  const remoteUrl_DS = `${appConfig.logaUrl_DS}`;
+  const jobs_DS = await fetchJobs(remoteUrl_DS);
+
+  let jobs = jobs_IS.concat(jobs_DS);
+
   const job = jobs.find((item) => item.id == params.id);
   return Response.json(job);
 }
