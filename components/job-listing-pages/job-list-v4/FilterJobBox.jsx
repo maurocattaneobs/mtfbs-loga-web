786affc84362f3b1be8b698b74a13c4c2814c214
Mauro Cattaneo 🍺 <mauro.cattaneo@mtfbs.com>
2025-02-24 17:47:15 +0100

    Fix imports

--- components/job-listing-pages/job-list-v4/FilterJobBox.jsx
+++ components/job-listing-pages/job-list-v4/FilterJobBox.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Link from "next/link";
+import Image from "next/image";
 
 const FilterJobBox = ({ jobs }) => {
   // Funzione per convertire base64 in Blob e aprirlo
