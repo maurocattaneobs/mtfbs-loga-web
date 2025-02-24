58b326a715443a2fccc41c259a154214cc252c47
Mauro Cattaneo 🍺 <mauro.cattaneo@mtfbs.com>
2025-02-24 17:38:50 +0100

    Update open file from base64 using blob

--- components/job-listing-pages/job-list-v4/FilterJobBox.jsx
+++ components/job-listing-pages/job-list-v4/FilterJobBox.jsx
@@ -1,69 +1,42 @@
 import React from "react";
-import Link from "next/link";
-import Image from "next/image";
 
 const FilterJobBox = ({ jobs }) => {
+  // Funzione per convertire base64 in Blob e aprirlo
+  const openBase64Pdf = (base64Data) => {
+    // Decodifica la stringa base64
+    const byteCharacters = atob(base64Data);
+    const byteNumbers = new Array(byteCharacters.length);
+    for (let i = 0; i < byteCharacters.length; i++) {
+      byteNumbers[i] = byteCharacters.charCodeAt(i);
+    }
+    const byteArray = new Uint8Array(byteNumbers);
+
+    // Crea un blob di tipo PDF
+    const blob = new Blob([byteArray], { type: "application/pdf" });
+
+    // Genera un object URL
+    const blobUrl = URL.createObjectURL(blob);
+
+    // Apri il blobUrl in una nuova scheda
+    window.open(blobUrl, "_blank", "noopener,noreferrer");
+  };
+
   let content = jobs?.map((item) => (
     <div className="job-block col-lg-6 col-md-12 col-sm-12" key={item.id}>
       <div className="inner-box">
         <div className="content">
-          <span className="company-logo">
-            <Image
-              width={50}
-              height={49}
-              src="/images/resource/placeholder.png"
-              alt="item brand"
-            />
-          </span>
-          <h4>
-            <Link href={`/job-single-v1/${item.index}`}>{item.title}</Link>
-          </h4>
+          {/* ... Altri campi ... */}
 
-          <ul className="job-info">
-            <li>
-              <span className="icon flaticon-briefcase"></span>
-              {item.shortName}
-            </li>
-            {/* compnay info */}
-            <li>
-              <span className="icon flaticon-map-locator"></span>
-              {item.location}
-            </li>
-            {/* location info */}
-            <li>
-              <span className="icon flaticon-clock-3"></span>{" "}
-              {item.time || "08:30 - 18:00"}
-            </li>
-            {/* time info */}
-            <li>
-              <span className="icon flaticon-money"></span>{" "}
-              {item.salary || "70/80 K"}
-            </li>
-            {/* salary info */}
-          </ul>
-          {/* End .job-info */}
-
-          <ul className="job-other-info">
-            {item?.jobType?.map((val, i) => (
-              <li key={i} className={`${val.styleClass}`}>
-                {val.type}
-              </li>
-            ))}
-          </ul>
-          {/* End .job-other-info */}
-
-          <a
-            href={`data:application/pdf;base64,${item.file1}`}
-            target="_blank"
-            rel="noopener noreferrer"
+          {/* Bottone per aprire il PDF in una nuova scheda */}
+          <button
             className="theme-btn btn-style-three bg-blue"
+            onClick={() => openBase64Pdf(item.file1)}
           >
-            <span className="btn-title">Apri il formulario</span>
-          </a>
+            Apri il formulario
+          </button>
         </div>
       </div>
     </div>
-    // End all jobs
   ));
 
   return (
