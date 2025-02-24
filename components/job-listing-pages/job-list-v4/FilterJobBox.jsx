b55cba5d2982c819a32e6cdfa583912164f2ccf3
Mauro Cattaneo 🍺 <mauro.cattaneo@mtfbs.com>
2025-02-24 17:43:39 +0100

    Update filter job box with all fileds

--- components/job-listing-pages/job-list-v4/FilterJobBox.jsx
+++ components/job-listing-pages/job-list-v4/FilterJobBox.jsx
@@ -25,7 +25,50 @@ const FilterJobBox = ({ jobs }) => {
     <div className="job-block col-lg-6 col-md-12 col-sm-12" key={item.id}>
       <div className="inner-box">
         <div className="content">
-          {/* ... Altri campi ... */}
+          <span className="company-logo">
+            <Image
+              width={50}
+              height={49}
+              src="/images/resource/placeholder.png"
+              alt="item brand"
+            />
+          </span>
+          <h4>
+            <Link href={`/job-single-v1/${item.index}`}>{item.title}</Link>
+          </h4>
+
+          <ul className="job-info">
+            <li>
+              <span className="icon flaticon-briefcase"></span>
+              {item.shortName}
+            </li>
+            {/* compnay info */}
+            <li>
+              <span className="icon flaticon-map-locator"></span>
+              {item.location}
+            </li>
+            {/* location info */}
+            <li>
+              <span className="icon flaticon-clock-3"></span>{" "}
+              {item.time || "08:30 - 18:00"}
+            </li>
+            {/* time info */}
+            <li>
+              <span className="icon flaticon-money"></span>{" "}
+              {item.salary || "70/80 K"}
+            </li>
+            {/* salary info */}
+          </ul>
+          {/* End .job-info */}
+
+          <ul className="job-other-info">
+            {item?.jobType?.map((val, i) => (
+              <li key={i} className={`${val.styleClass}`}>
+                {val.type}
+              </li>
+            ))}
+          </ul>
+          {/* End .job-other-info */}
 
           {/* Bottone per aprire il PDF in una nuova scheda */}
           <button
